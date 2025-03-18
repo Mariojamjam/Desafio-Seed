6a3090ae670db515ded7e7ba4febcfba0e0dd0dd
MarioJam <mj.andrade.moreira@gmail.com>
2025-03-18 12:57:00 -0300

    feat

--- main.js
+++ main.js
@@ -2,15 +2,22 @@ let boneco = document.querySelector("#boneco")
 let tela = document.querySelector("#tela_1")
 let plataformas = document.querySelectorAll(".plataformas")
 
+let inimigos = document.querySelectorAll(".inimigos")
+let velocidade_inimigo = 2
+let direcao_inimigo = 1
+
 let velocidade_x = 0
 let velocidade_y = 0
 let gravidade = 1
 let velocidadeMAX = 20
 let noChao = false
 let tamanho_pulo = -25
+let ricochete = -18
 let movimento_distancia = 5
 
 let teclas_pressionadas = {}
+
+
 
 window.addEventListener("keydown", (event) => {
     teclas_pressionadas[event.key] = true
@@ -20,10 +27,17 @@ window.addEventListener("keyup", (event) => {
     teclas_pressionadas[event.key] = false
 })
 
-function movimento(){
+//contador de tempo para alterar a direção do inimigo
+setInterval(() => {
+    direcao_inimigo *= -1; 
+}, 4000);  
+
+function interacoes(){
+
+    //movimentação do player
     let boneco_pos = boneco.getBoundingClientRect()
     let tela_pos = tela.getBoundingClientRect()
-
+    
     if (teclas_pressionadas["ArrowLeft"] && boneco_pos.left >tela_pos.left){
         velocidade_x = -movimento_distancia
     }
@@ -58,6 +72,8 @@ function movimento(){
         novaPosX = tela.offsetWidth - boneco.offsetWidth
     }
 
+
+    //colisão com plataformas
     noChao=false
     plataformas.forEach((plataformas) => {
         let plataformas_pos = plataformas.getBoundingClientRect()
@@ -69,7 +85,7 @@ function movimento(){
         )
             
             {
-            novaPosY = plataformas_pos.top - ((plataformas_pos.bottom - plataformas_pos.top)+13)
+            novaPosY = plataformas_pos.top - boneco.offsetHeight/1.1
             noChao = true
             velocidade_y=0
             }
@@ -91,7 +107,7 @@ function movimento(){
         if(boneco_pos.right +velocidade_x >= plataformas_pos.left&&
             boneco_pos.bottom > plataformas_pos.top +8&&
             boneco_pos.top < plataformas_pos.bottom -8&&
-            boneco_pos.left < plataformas_pos.left
+            boneco.offsetLeft+boneco.offsetWidth <= plataformas_pos.left
         ){
             novaPosX = plataformas_pos.left- (boneco_pos.right - boneco_pos.left)
             velocidade_x =0
@@ -101,7 +117,7 @@ function movimento(){
         if(boneco_pos.left +velocidade_x <= plataformas_pos.right&&
             boneco_pos.bottom > plataformas_pos.top+8&&
             boneco_pos.top < plataformas_pos.bottom -8&&
-            boneco_pos.right > plataformas_pos.right
+            boneco.offsetLeft >= plataformas_pos.right
         ){
             novaPosX = plataformas_pos.right
             velocidade_x =0
@@ -110,16 +126,59 @@ function movimento(){
     
     })
 
+    //interações com inimigos
+    inimigos.forEach((inimigos)=> {
+        let inimigos_pos = inimigos.getBoundingClientRect()
+        if (
+            boneco_pos.bottom + velocidade_y >= inimigos_pos.top &&
+            boneco_pos.top < inimigos_pos.top &&
+            boneco_pos.right > inimigos_pos.left &&
+            boneco_pos.left < inimigos_pos.right
+        ){
+            inimigos.style.display = "none"
+            velocidade_y = ricochete
+            novaPosY = inimigos_pos.top-100
+
+        }
+
+        if(boneco_pos.right +velocidade_x >= inimigos_pos.left&&
+            boneco_pos.bottom > inimigos_pos.top &&
+            boneco_pos.top < inimigos_pos.bottom &&
+            boneco.offsetLeft+boneco.offsetWidth <= inimigos_pos.left
+        ){
+            //novaPosX = inimigos_pos.left- (boneco_pos.right - boneco_pos.left)-200  
+            velocidade_x =0
+            direcao_inimigo *= -1
+
+        }
+
+        if(boneco_pos.left +velocidade_x <= inimigos_pos.right&&
+            boneco_pos.bottom > inimigos_pos.top &&
+            boneco_pos.top < inimigos_pos.bottom &&
+            boneco.offsetLeft >= inimigos_pos.right
+        ){
+            //novaPosX = inimigos_pos.right+100
+            direcao_inimigo *= -1
+            velocidade_x =0
+        }
+
+        let novaPosX = inimigos.offsetLeft + velocidade_inimigo * direcao_inimigo
+        inimigos.style.left = novaPosX + "px"
+
+    })
+
     if (novaPosY + boneco.offsetHeight > tela.offsetHeight){
         novaPosY = tela.offsetHeight - boneco.offsetHeight
         noChao = true
         velocidade_y = 0
     }
 
+    
+
     boneco.style.left = novaPosX + "px";
     boneco.style.top = novaPosY + "px";
 
-    requestAnimationFrame(movimento)
+    requestAnimationFrame(interacoes)
 }
 
-movimento()
+interacoes()
